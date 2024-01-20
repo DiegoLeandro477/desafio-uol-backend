c0cc86cdb03504f2a59c439265fe5eb3997a9df2
DiegoLeandro477 <diegoleandro477@gmail.com>
2024-01-20 13:14:10 -0300

    Config Complete

---
+++ src/test/java/br/com/ferruje/desafiouolbackendspirng/DesafioUolBackendSpirngApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferruje.desafiouolbackendspirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUolBackendSpirngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
