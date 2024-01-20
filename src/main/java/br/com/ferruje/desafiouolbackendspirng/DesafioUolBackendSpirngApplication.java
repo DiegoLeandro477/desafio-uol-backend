19d92ebe8d50d37391c031485c3b2c08f49f1523
DiegoLeandro477 <diegoleandro477@gmail.com>
2024-01-20 13:15:29 -0300

    Config Finish

---
+++ src/main/java/br/com/ferruje/desafiouolbackendspirng/DesafioUolBackendSpirngApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferruje.desafiouolbackendspirng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUolBackendSpirngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUolBackendSpirngApplication.class, args);
+	}
+
+}
