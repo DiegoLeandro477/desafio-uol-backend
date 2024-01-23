ccb51ce2dfe6960dce4b031f7b07dbbe1749edff
DiegoLeandro477 <diegoleandro477@gmail.com>
2024-01-23 17:39:10 -0300

    config CrossOrigin

--- src/main/java/br/com/ferruje/desafiouolbackendspirng/controllers/UserResouce.java
+++ src/main/java/br/com/ferruje/desafiouolbackendspirng/controllers/UserResouce.java
@@ -14,10 +14,12 @@ import br.com.ferruje.desafiouolbackendspirng.services.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:8081")
 public class UserResouce {
 
     @Autowired
